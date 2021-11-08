6462a14da9dfdc18169dd9d261306c4eb5a4b59b
muminecan <javamuminecan@gmail.com>
2021-11-07 19:55:06 -0600

    add

---
+++ src/javadevelopmentday02/MultiThreading02.java
@@ -0,0 +1,33 @@
+package javadevelopmentday02;
+
+public class MultiThreading02 {
+    public static int counter = 0;
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Counter.count();
+                System.out.println("thread1 is completed");
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Counter.count();
+                System.out.println("thread2 is completed");
+            }
+        });
+        thread2.start();
+
+    }
+}
+class  Counter{
+    synchronized public static void count() {
+        for(int i=1; i<=1000; i++) {
+            MultiThreading02.counter++;
+        }
+        System.out.println("Counter: " + MultiThreading02.counter);
+    }
+}
