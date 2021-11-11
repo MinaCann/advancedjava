a27a1a36f2736000f048176625370ff37a54487b
muminecan <javamuminecan@gmail.com>
2021-11-11 08:51:01 -0600

    add more code

---
+++ src/javadevelopmentDay03/WaitNotify.java
@@ -0,0 +1,52 @@
+package javadevelopmentDay03;
+
+/*
+   In Volatile, a thread depends on variables to run, Where as in WaitNotify, thread depends on another thread.
+ */
+public class WaitNotify {
+    
+	static public double balance =0;
+	public static void main(String[] args) {
+	
+		WaitNotify obj = new WaitNotify();
+		Thread thread1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				obj.withdraw(800);
+			}	
+		});
+		thread1.start();		
+		
+        Thread thread2 = new Thread(new Runnable() {
+        	@Override
+        	public void run() {
+        		
+        		obj.deposit(2000);
+        	}
+        });
+        thread2.start();
+	}
+	public void withdraw(double amount) {
+		 synchronized(this) {
+		if(balance <= 0||balance<amount) {
+			try {
+				System.out.println("Waiting for the balance to be updated");
+				wait();
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		 }
+		balance=balance-amount;
+		System.out.println("Withdrawal is successful.\nThe current balance is " + balance);
+	}
+     public void deposit(double amount) {
+    	 
+    	 balance= balance+amount;
+    	 
+    	 System.out.println("The balance is updated.\nThe current balance is" + balance);
+    	 notify();
+    	 synchronized(this) {notify();
+    	 }
+     }
+}
